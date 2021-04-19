ac7292812e8382c02267c17c3947659eb46dd195
VHromyk <hromykk@gmail.com>
2021-04-20 01:15:19 +0300

    finished rewriting project to hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,32 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './Modal.scss';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal({ children, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return window.removeEventListener('keydown', handleKeyDown);
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+  const handleKeyDown = event => {
+    if ((event.code = 'Escape')) {
+      onClose();
     }
   };
 
-  handleBackdrop = event => {
+  const handleBackdrop = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return (
-      <div className="Overlay" onClick={this.handleBackdrop}>
-        <div className="Modal">{this.props.children}</div>
-      </div>
-    );
-  }
-}
 
-export default Modal;
+  return (
+    <div className="Overlay" onClick={handleBackdrop}>
+      <div className="Modal">{children}</div>
+    </div>
+  );
+}
